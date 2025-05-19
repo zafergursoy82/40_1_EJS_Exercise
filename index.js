96e6136fb61a2badfb5a46b26463ca773b1300b9
zafergursoy82 <zafergursoyy1982@gmail.com>
2025-05-19 15:00:06 +0300

    update

--- index.js
+++ index.js
@@ -4,18 +4,28 @@ const app = express();
 const port = 3000;
 
 var dayOfTheWeek = "";
+var dayTypeOfWeek = "";
+var adviceOfDay = "";
 
-function getDay() {
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const today = new Date();
-    dayOfTheWeek = days[today.getDay()];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const today = new Date("August 28, 2082 11:00:00");
+dayOfTheWeek = days[today.getDay()];
+
+if (dayOfTheWeek === "Sunday" || dayOfTheWeek === "Saturday") {
+    dayTypeOfWeek = "weekend";
+    adviceOfDay = "its time to enjoy, relaxxx";
+} else {
+    dayTypeOfWeek = "a weekday";
+    adviceOfDay = "its time to work hard"; 
 }
-
-getDay();
 
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { day: dayOfTheWeek });
+    res.render("index.ejs", { 
+        day: dayOfTheWeek,
+        dayType: dayTypeOfWeek,
+        advice: adviceOfDay
+    });
 });
 
 
